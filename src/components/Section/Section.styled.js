3bb8270dc78bd89f595b31d4808b8db8b9839d03
SSSerg05 <ssserg05@gmail.com>
2023-06-22 13:06:29 +0300

    fix styles

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -4,7 +4,7 @@ export const Title = styled.h2`
   margin: 0;
   padding: 0;
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 48px;
+  font-size: 36px;
   font-weight: 700;
   letter-spacing: 0.05rem;
 `
