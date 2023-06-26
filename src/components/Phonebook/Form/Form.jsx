11d1fc8a91e7fc8fc796b01b656259c9edfb3abc
SSSerg05 <ssserg05@gmail.com>
2023-06-26 16:00:52 +0300

    fix styles input+label

--- src/components/Phonebook/Form/Form.jsx
+++ src/components/Phonebook/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 
+import { FieldBox, FieldLabel, FieldPosition, FieldInput } from "./Form.styled"
 
 export class Form extends Component {
   static defaultProps = {};
@@ -42,33 +43,40 @@ export class Form extends Component {
     const { name, phone } = this.state;
 
     return (
-      <form onSubmit={ this.handleSubmit }>
-        <label>
-          Name
+      <form onSubmit={this.handleSubmit}>
+        <FieldBox>
+          <FieldLabel>
+            Name
+            <FieldPosition>
+              <FieldInput
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value={ name }
+                onChange={ this.handleChange }
+              />
+            </FieldPosition>
+          </FieldLabel>
+        </FieldBox>
 
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={ name }
-            onChange={ this.handleChange }
-          />
-        </label>
-
-        <label>
-          Phone
-          <input
-            type="tel"
-            name="phone"
-            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={ phone }
-            onChange={ this.handleChange }
-          />
-        </label>
+        <FieldBox>
+          <FieldLabel>
+            Phone
+            <FieldPosition>
+              <FieldInput
+                type="tel"
+                name="phone"
+                pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                value={ phone }
+                onChange={ this.handleChange }
+              />
+            </FieldPosition>
+          </FieldLabel>
+        </FieldBox>
 
         <button type="submit">Sign up as { name }</button>
       </form>
--- src/components/Phonebook/Form/Form.styled.jsx
+++ src/components/Phonebook/Form/Form.styled.jsx
@@ -1,1 +1,36 @@
 import styled from "@emotion/styled";
+
+export const FieldBox = styled.div`
+  position: relative;
+  margin-bottom: 8px;
+`
+
+export const FieldLabel = styled.label`
+  display: block;
+  padding: 0 4px;
+`
+
+export const FieldInput = styled.input`
+    display: block;
+
+    width: 100%;
+    /* height: 40px; */
+    outline: 1px solid rgba(33, 33, 33, 0.2);
+    background-color: transparent;
+    border-radius: 4px;
+    border: none;
+
+    padding: 8px;
+`
+
+export const FieldPosition = styled.span`
+  position: relative;
+  margin-top: 4px;
+
+
+  &:hover > .FieldInput, 
+  &:focus-within > .FieldInput {
+    outline: 1px solid blue;
+    transition: outline 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+`
